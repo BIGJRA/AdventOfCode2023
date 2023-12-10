dfab97ce90a6cf5a2ab4f80b7076ed70322d07be
BIGJRA <47573834+BIGJRA@users.noreply.github.com>
2023-12-10 18:52:12 -0500

    Template files

---
+++ 2023_19.py
@@ -0,0 +1,26 @@
+from aocd import get_data, post
+from common import *
+
+
+def solve(data, p):
+    lines = data.splitlines()
+    return p
+
+s1 = '''
+'''
+
+tests = {
+    s1: (0, 0)
+}
+
+test_assertions(tests, solve)
+
+input_data = get_data(day=19, year=2023)
+
+p1 = solve(input_data, p=1)
+print(f"Part 1: {p1}")
+# post.submit(p1, part=1, day=19, year=2023)
+
+# p2 = solve(input_data, p=2)
+# print(f"Part 2: {p2}")
+# post.submit(p2, part=2, day=19, year=2023)
