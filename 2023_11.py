40a617f0c015ca11bb6ff4f867b1a41ac3e8ec65
BIGJRA <47573834+BIGJRA@users.noreply.github.com>
2023-12-12 13:12:54 -0500

    1112

--- 2023_11.py
+++ 2023_11.py
@@ -4,13 +4,52 @@ from common import *
 
 def solve(data, p):
     lines = data.splitlines()
-    return p
+    m, n = len(lines[0]), len(lines)
+    galaxies = []
+    for y, line in enumerate(lines):
+        for x, char in enumerate(line):
+            if char == "#":
+                galaxies.append((x,y))
+    empty_rows = []
+    empty_cols = []
+    for r in range(n):
+        if not any([g[1] == r for g in galaxies]):
+            empty_rows.append(r)
+    for c in range(m):
+        if not any([g[0] == c for g in galaxies]):
+            empty_cols.append(c)
+    total = 0
+    distance_multipliers = [2, 1000000]
+    for pos, g1 in enumerate(galaxies):
+        for g2 in galaxies[pos + 1:]:
+            distance = 0
+            for h_move in range(min(g1[0],g2[0]), max(g1[0],g2[0])):
+                if h_move in empty_cols:
+                    distance += distance_multipliers[p-1]
+                else:
+                    distance += 1
+            for v_move in range(min(g1[1],g2[1]), max(g1[1],g2[1])):
+                if v_move in empty_rows:
+                    distance += distance_multipliers[p-1]
+                else:
+                    distance += 1
+            total += distance
 
-s1 = '''
-'''
+    return total
+
+s1 = '''...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....'''
 
 tests = {
-    s1: (0, 0)
+    s1: (374, None)
 }
 
 test_assertions(tests, solve)
@@ -21,6 +60,6 @@ p1 = solve(input_data, p=1)
 print(f"Part 1: {p1}")
 # post.submit(p1, part=1, day=11, year=2023)
 
-# p2 = solve(input_data, p=2)
-# print(f"Part 2: {p2}")
+p2 = solve(input_data, p=2)
+print(f"Part 2: {p2}")
 # post.submit(p2, part=2, day=11, year=2023)
--- 2023_12.py
+++ 2023_12.py
@@ -1,16 +1,61 @@
+import re
+import time
+from collections import deque
+from functools import lru_cache
+from itertools import combinations
+
 from aocd import get_data, post
 from common import *
 
 
 def solve(data, p):
     lines = data.splitlines()
-    return p
+    total = 0
+    for line in lines:
+        springs, blocks = line.split()
+        blocks = tuple([int(i) for i in blocks.split(',')])
+        if p == 2:
+            springs = ((springs + '?') * 5)[:-1]  # multiply by 5, shear off last '?'
+            blocks *= 5
+        m, n = len(springs), len(blocks)
+        cache = {}
 
-s1 = '''
-'''
+        def dp(spring_idx, block_idx, curr_count):
+            if (spring_idx, block_idx, curr_count) in cache:
+                return cache[(spring_idx, block_idx, curr_count)]
+            if spring_idx == m:
+                if (block_idx == n and curr_count == 0) or (block_idx == n - 1 and curr_count == blocks[block_idx]):
+                    return 1
+                else:
+                    return 0
+            ans = 0
+            for poss in [i for i in ['.', '#'] if i != {'.': '#', '#': '.'}.get(springs[spring_idx])]:
+                if poss == '.':
+                    if curr_count == 0:
+                        ans += dp(spring_idx + 1, block_idx, 0)
+                    elif curr_count > 0 and block_idx < n and curr_count == blocks[block_idx]:
+                        ans += dp(spring_idx + 1, block_idx + 1, 0)
+                elif poss == '#':
+                    ans += dp(spring_idx + 1, block_idx, curr_count + 1)
+            cache[(spring_idx, block_idx, curr_count)] = ans
+            return ans
+
+        score = dp(0, 0, 0)
+        total += score
+        continue
+
+    return total
+
+
+s1 = '''???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1'''
 
 tests = {
-    s1: (0, 0)
+    s1: (21, 525152)
 }
 
 test_assertions(tests, solve)
@@ -21,6 +66,6 @@ p1 = solve(input_data, p=1)
 print(f"Part 1: {p1}")
 # post.submit(p1, part=1, day=12, year=2023)
 
-# p2 = solve(input_data, p=2)
-# print(f"Part 2: {p2}")
+p2 = solve(input_data, p=2)
+print(f"Part 2: {p2}")
 # post.submit(p2, part=2, day=12, year=2023)
