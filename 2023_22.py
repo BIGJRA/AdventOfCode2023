5982c51a53d8be56c9a81dfc8c300cb730ad9e27
BIGJRA <47573834+BIGJRA@users.noreply.github.com>
2023-12-23 20:19:10 -0800

    22 and 23 done

--- 2023_22.py
+++ 2023_22.py
@@ -1,16 +1,65 @@
+import re
+from collections import deque
+from copy import deepcopy
+
 from aocd import get_data, post
 from common import *
 
 
 def solve(data, p):
     lines = data.splitlines()
-    return p
+    bricks = []
+    for line in lines:
+        tx0, ty0, tz0, tx1, ty1, tz1 = map(int, re.findall(r'\d+', line))
+        bricks.append(sorted([[tx0, ty0, tz0], [tx1, ty1, tz1]]))
+    n = len(bricks)
+    bricks.sort(key=lambda x: x[0][2])  # bricks start in sorted by height order so they fall greedily
 
-s1 = '''
-'''
+    # max_heights = defaultdict(lambda: (0, -1))  # based on (x,y), starts at (z=0,block_idx=-1)
+    tops = {(x, y): (0, -1) for x in range(10) for y in range(10)}  # input has 0 <= x,y < 10, no need for DD
+    supports = [set() for _ in range(n)]
+    in_degrees = [0 for _ in range(n)]
+    for pos, ((x0, y0, z0), (x1, y1, z1)) in enumerate(bricks):
+        curr_tops = [tops[x, y] for x in range(x0, x1 + 1) for y in range(y0, y1 + 1)]
+        target_z = max(u[0] + 1 for u in curr_tops)  # one above the tallest occupied point under our brick
+        # filter for neighbors only includes the blocks that are one height less than target
+        neighbors = {f[1] for f in filter(lambda u: u[1] != -1 and u[0] == target_z - 1, curr_tops)}
+        for adj in neighbors:
+            supports[adj].add(pos)
+            in_degrees[pos] += 1
+        for x in range(x0, x1 + 1):
+            for y in range(y0, y1 + 1):
+                tops[(x, y)] = (target_z + (z1 - z0), pos)
+    frees = 0
+    cascades = 0
+    for idx in range(n):
+        in_deg = deepcopy(in_degrees)
+        count = 0
+        q = deque([idx])
+        while q:  # we only care about what happens when blocks lose their single supports - Queue tracks only these
+            curr = q.popleft()
+            for neighbor in supports[curr]:
+                in_deg[neighbor] -= 1
+                if in_deg[neighbor] == 0:
+                    count += 1
+                    q.append(neighbor)
+        if count:  # P2 wants to see impact of deleting blocks - if >= 1 it is relevant here
+            cascades += count
+        else:  # P1 wants to see how many blocks have no impact
+            frees += 1
+    return frees if p == 1 else cascades
+
+
+s1 = '''1,0,1~1,2,1
+0,0,2~2,0,2
+0,2,3~2,2,3
+0,0,4~0,2,4
+2,0,5~2,2,5
+0,1,6~2,1,6
+1,1,8~1,1,9'''
 
 tests = {
-    s1: (0, None)
+    s1: (5, 7),
 }
 
 test_assertions(tests, solve)
@@ -21,6 +70,6 @@ p1 = solve(input_data, p=1)
 print(f"Part 1: {p1}")
 # post.submit(p1, part=1, day=22, year=2023)
 
-# p2 = solve(input_data, p=2)
-# print(f"Part 2: {p2}")
+p2 = solve(input_data, p=2)
+print(f"Part 2: {p2}")
 # post.submit(p2, part=2, day=22, year=2023)
--- 2023_23.py
+++ 2023_23.py
@@ -1,16 +1,96 @@
+from collections import deque, defaultdict
+from copy import deepcopy
+
 from aocd import get_data, post
 from common import *
 
 
 def solve(data, p):
     lines = data.splitlines()
-    return p
+    num_rows, num_cols = len(lines), len(lines[0])
+    start, end = (0,1), (num_rows - 1, num_cols - 2)
 
-s1 = '''
-'''
+    features = {(-1, 1): "#", (num_rows, num_cols - 2): "#"}
+    for row, line in enumerate(lines):
+        for col, char in enumerate(line):
+            features[(row, col)] = char
+    graph = {start: set(), end: set()}
+    for row, col in features:
+        if row == 0 or row == num_rows - 1 or col == 0 or col == num_cols:
+            continue
+        if features[(row, col)] != "#":
+            count = sum([features[(row + r0, col + c0)] != '#' for (r0, c0) in ((0, 1), (1, 0), (-1, 0), (0, -1))])
+            if count >= 3:
+                graph[(row, col)] = set()
+    start = (0, 1)
+    q = deque([((1, 1), start, 1, (0, 1))])
+    slopes = {"<": (0, -1), ">": (0, 1), "^": (-1, 0), "v": (1, 0)}
+    while q:
+        coords, last_junction, steps, prev = q.popleft()
+        if coords in graph:
+            graph[last_junction].add((coords, steps))
+            if coords == end:
+                continue
+            for slope, d in slopes.items():
+                new = coords[0] + d[0], coords[1] + d[1]
+                if features[new] == '#' or new == prev:
+                    continue
+                q.append((new, coords, 1, coords))
+        else:
+            for slope, d in slopes.items():
+                new = coords[0] + d[0], coords[1] + d[1]
+                if features[new] == '#' or new == prev:
+                    continue
+                if features[coords] in slopes and slopes[features[coords]] != d:
+                    continue
+                q.append((new, last_junction, steps + 1, coords))
+    if p == 2:
+        for g in graph:
+            if g != start:
+                for n, d in graph[g]:
+                    graph[n].add((g, d))
+    q = deque([(start, 0, set())])
+    best = 0
+    while q:
+        curr, steps, vis = q.popleft()
+        if curr == end:
+            if steps > best:
+                best = steps
+            continue
+        new_vis = vis.copy()
+        new_vis.add(curr)
+        for adj, d in graph[curr]:
+            if adj in vis:
+                continue
+            q.append((adj, steps + d, new_vis))
+    return best
+
+s1 = '''#.#####################
+#.......#########...###
+#######.#########.#.###
+###.....#.>.>.###.#.###
+###v#####.#v#.###.#.###
+###.>...#.#.#.....#...#
+###v###.#.#.#########.#
+###...#.#.#.......#...#
+#####.#.#.#######.#.###
+#.....#.#.#.......#...#
+#.#####.#.#.#########v#
+#.#...#...#...###...>.#
+#.#.#v#######v###.###v#
+#...#.>.#...>.>.#.###.#
+#####v#.#.###v#.#.###.#
+#.....#...#...#.#.#...#
+#.#########.###.#.#.###
+#...###...#...#...#.###
+###.###.#.###v#####v###
+#...#...#.#.>.>.#.>.###
+#.###.###.#.###.#.#v###
+#.....###...###...#...#
+#####################.#'''
 
 tests = {
-    s1: (0, None)
+    s1: (94, 154)
 }
 
 test_assertions(tests, solve)
@@ -21,6 +101,6 @@ p1 = solve(input_data, p=1)
 print(f"Part 1: {p1}")
 # post.submit(p1, part=1, day=23, year=2023)
 
-# p2 = solve(input_data, p=2)
-# print(f"Part 2: {p2}")
+p2 = solve(input_data, p=2)
+print(f"Part 2: {p2}")
 # post.submit(p2, part=2, day=23, year=2023)
